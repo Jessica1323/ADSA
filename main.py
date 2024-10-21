539b8cb808ba9066d7402d190c28e848757efe9c
zihan-Luo <162454993+luozihan27@users.noreply.github.com>
2024-10-21 17:26:52 +1030

    main

--- main.py
+++ main.py
@@ -1,75 +1,97 @@
+
+def letter_to_cost(letter):
+    if 'A' <= letter <= 'Z':
+        return ord(letter) - ord('A')  
+    elif 'a' <= letter <= 'z':
+        return ord(letter) - ord('a') + 26  
+    else:
+        return float('inf')  
+
+
+def parse_input(input_string):
+    parts = input_string.strip().split(' ')
+    country_str, build_str, destroy_str = parts[0], parts[1], parts[2]
+    
+ 
+    country = [list(map(int, row)) for row in country_str.split(',')]
+    build = [list(row) for row in build_str.split(',')]
+    destroy = [list(row) for row in destroy_str.split(',')]
+    
+    return country, build, destroy
+
+
 class UnionFind:
     def __init__(self, n):
         self.parent = list(range(n))
-        self.rank = [1] * n
+        self.rank = [0] * n
+
 
     def find(self, p):
         if self.parent[p] != p:
             self.parent[p] = self.find(self.parent[p])
         return self.parent[p]
 
+
     def union(self, p, q):
         rootP = self.find(p)
         rootQ = self.find(q)
-        if rootP != rootQ:
-            if self.rank[rootP] > self.rank[rootQ]:
-                self.parent[rootQ] = rootP
-            elif self.rank[rootP] < self.rank[rootQ]:
-                self.parent[rootP] = rootQ
-            else:
-                self.parent[rootQ] = rootP
-                self.rank[rootP] += 1
+
+        if rootP == rootQ:
+            return False
 
 
-def char_to_cost(c):
-    cost_map = {
-        'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
-        'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
-        'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25,
-        'a': 26, 'b': 27, 'c': 28, 'd': 29, 'e': 30, 'f': 31, 'g': 32, 'h': 33, 'i': 34, 'j': 35,
-        'k': 36, 'l': 37, 'm': 38, 'n': 39, 'o': 40, 'p': 41, 'q': 42, 'r': 43, 's': 44, 't': 45,
-        'u': 46, 'v': 47, 'w': 48, 'x': 49, 'y': 50, 'z': 51
-    }
-    
-    return cost_map.get(c, float('inf')) 
+        if self.rank[rootP] > self.rank[rootQ]:
+            self.parent[rootQ] = rootP
+        elif self.rank[rootP] < self.rank[rootQ]:
+            self.parent[rootP] = rootQ
+        else:
+            self.parent[rootQ] = rootP
+            self.rank[rootP] += 1
+
+        return True
 
 
-def min_cost_reconstruction(n, country, build, destroy):
-    edges = []
-    
-    # Collect all edges (existing roads and new roads to build)
-    for i in range(n):
-        for j in range(i + 1, n):
-            if country[i][j] == '1':  # Existing road, consider destroying
-                destroy_cost = char_to_cost(destroy[i][j])
-                edges.append((destroy_cost, i, j, "destroy"))
-            if country[i][j] == '0':  # No road, consider building
-                build_cost = char_to_cost(build[i][j])
-                edges.append((build_cost, i, j, "build"))
-    
-    # Sort edges by cost (Kruskal algorithm)
-    edges.sort()
+def kruskal(n, country, build, destroy):
 
     uf = UnionFind(n)
-    total_cost = 0
-    
-    # Apply Kruskal's algorithm to find the MST
-    for cost, u, v, action in edges:
-        if uf.find(u) != uf.find(v):  # If u and v are not connected
-            uf.union(u, v)
-            total_cost += cost
-            if action == "destroy":
-                print(f"Destroying road between {u} and {v}, cost: {cost}")
-            elif action == "build":
-                print(f"Building road between {u} and {v}, cost: {cost}")
-    
-    return total_cost
+    edges = []
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if country[i][j] == 1:
+
+                cost = letter_to_cost(destroy[i][j])
+                edges.append((i, j, cost, 'destroy'))
+            else:
+
+                cost = letter_to_cost(build[i][j])
+                edges.append((i, j, cost, 'build'))
 
 
-n = 3
-country = ["011", "101", "110"]
-build = ["ABD", "BAC", "DCA"]
-destroy = ["ABD", "BAC", "DCA"]
+    edges = sorted(edges, key=lambda x: x[2])
 
-result = min_cost_reconstruction(n, country, build, destroy)
-print(result)  
+    mst_weight = 0  
+    connected_components = n  
+
+
+    for u, v, weight, action in edges:
+        if uf.union(u, v): 
+            mst_weight += weight
+            connected_components -= 1
+
+    return mst_weight, connected_components
+
+
+def process_input(input_string):
+    country, build, destroy = parse_input(input_string)
+    n = len(country)  
+
+    mst_weight, connected_components = kruskal(n, country, build, destroy)
+
+
+    print(f"Minimum Spanning Tree Weight: {mst_weight}")
+    print(f"Number of connected components: {connected_components}")
+
+
+# input_string = "000,000,000 ABD,BAC,DCA ABD,BAC,DCA"
+# process_input(input_string)
