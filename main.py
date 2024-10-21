cb641bc3a7d51f1933019dc4a1530dc1d5242a9f
Jessica1323 <simon_tula@163.com>
2024-10-22 01:11:23 +1030

    11

--- main.py
+++ main.py
@@ -1,179 +1,83 @@
-class Node:
-    def __init__(self, links, build_cost, destroy_cost):
-        self.distance = 0  # 初始距离为0，用于Jarnik-Prim算法
-        self.links = links  # 存储节点之间的道路
-        self.build_cost = build_cost  # 建造成本
-        self.destroy_cost = destroy_cost  # 破坏成本
+def letter_to_cost(letter):
+    if 'A' <= letter <= 'Z':
+        return ord(letter) - ord('A')
+    elif 'a' <= letter <= 'z':
+        return ord(letter) - ord('a') + 26
+    else:
+        return float('inf')
 
-    # 获取和设置函数
-    def get_distance(self):
-        return self.distance
-
-    def set_distance(self, new_distance):
-        self.distance = new_distance
-
-    def get_links(self):
-        return self.links
-
-    def set_links(self, new_links):
-        self.links = new_links
-
-    def get_build_costs(self):
-        return self.build_cost
-
-    def set_build_costs(self, new_build_costs):
-        self.build_cost = new_build_costs
-
-    def get_destroy_costs(self):
-        return self.destroy_cost
-
-    def set_destroy_costs(self, new_destroy_costs):
-        self.destroy_cost = new_destroy_costs
-
-
-class Map:
-    def __init__(self, new_nodes):
-        self.nodes = new_nodes
-        self.running_cost = 0
-
-    def connect_nodes(self):
-        if len(self.nodes) == 1:
-            self.running_cost = 0
-            return
-
-        new_nodes = self.nodes[:]
-        curr_node = self.nodes[0]
-        the_map = [curr_node]
-        self.nodes[0] = None
-        min_build_cost = 53
-        map_indexer = 0
-        link_existed = 0
-        min_node = None
-        parent_node = None
-        index = -1
-
-        while curr_node is not None:
-            for i in range(len(curr_node.get_links())):
-                if curr_node.get_links()[i] == '1':
-                    if self.nodes[i] is not None:
-                        the_map.append(self.nodes[i])
-                        self.nodes[i] = None
-                        link_existed = 1
-                elif curr_node.get_links()[i] == '0' and self.nodes[i] is not None:
-                    cost = curr_node.get_build_costs()[i]
-                    cost_ascii = ord(cost) - 65 if ord(cost) <= 90 else ord(cost) - 71
-                    if cost_ascii < min_build_cost:
-                        min_build_cost = cost_ascii
-                        min_node = self.nodes[i]
-                        parent_node = curr_node
-                        index = i
-
-            if link_existed == 0 and len(the_map) == map_indexer + 1:
-                the_map.append(min_node)
-                self.nodes[index] = None
-                new_link = list(parent_node.get_links())
-                new_link[index] = '1'
-                parent_node.set_links(''.join(new_link))
-
-                new_link = list(min_node.get_links())
-                for i in range(len(new_nodes)):
-                    if parent_node.get_build_costs() == new_nodes[i].get_build_costs():
-                        index = i
-
-                new_link[index] = '1'
-                min_node.set_links(''.join(new_link))
-                map_indexer = 0
-                link_existed = 0
-                self.running_cost += min_build_cost
-                min_build_cost = 53
-            elif link_existed == 0 and len(the_map) > map_indexer + 1:
-                map_indexer += 1
-                curr_node = the_map[map_indexer]
-                continue
-            elif link_existed == 1:
-                map_indexer = 0
-                min_build_cost = 53
-                link_existed = 0
-
-            finished = all(node is None for node in self.nodes)
-            if finished:
-                curr_node = None
-            else:
-                parent_node = None
-                curr_node = the_map[map_indexer]
-
-        self.nodes = new_nodes
-
-    def delete_roads(self):
-        if len(self.nodes) == 1:
-            self.running_cost = 0
-            return
-
-        curr_node = self.nodes[0]
-        max_dest_cost = 0
-        final_dest_cost = 0
-        priority_queue = [curr_node]
-
-        while curr_node is not None:
-            if curr_node.get_distance() != 53:
-                max_dest_cost += curr_node.get_distance()
-
-            priority_queue.pop()
-            curr_node.set_distance(53)
-
-            for i in range(len(curr_node.get_links())):
-                if curr_node.get_links()[i] == '1':
-                    dest_cost = curr_node.get_destroy_costs()[i]
-                    cost_ascii = ord(dest_cost) - 65 if ord(dest_cost) <= 90 else ord(dest_cost) - 71
-                    if cost_ascii > self.nodes[i].get_distance() or (cost_ascii == 0 and self.nodes[i].get_distance() == 0):
-                        self.nodes[i].set_distance(cost_ascii)
-                        final_dest_cost += self.nodes[i].get_distance()
-
-                        priority_queue = [node for node in priority_queue if node.get_build_costs() != self.nodes[i].get_build_costs()]
-                        inserted = False
-                        for j, node in enumerate(priority_queue):
-                            if self.nodes[i].get_distance() < node.get_distance():
-                                priority_queue.insert(j, self.nodes[i])
-                                inserted = True
-                                break
-                        if not inserted:
-                            priority_queue.append(self.nodes[i])
-                    elif cost_ascii < self.nodes[i].get_distance() and self.nodes[i].get_distance() != 53:
-                        final_dest_cost += cost_ascii
-
-            if not priority_queue:
-                curr_node = None
-            else:
-                curr_node = priority_queue[-1]
-
-        self.running_cost += final_dest_cost - max_dest_cost
-
-    def print_cost(self):
-        print(self.running_cost)
-
-
-def main():
-   
-    # 读取输入
-    input_line = input().strip()
-    tokens = input_line.split(',')
+def parse_input(input_string):
+    parts = input_string.strip().split(' ')
+    country_str, build_str, destroy_str = parts[0], parts[1], parts[2]
     
-    build_line = input().strip()
-    build = build_line.split(',')
-
-    destroy_line = input().strip()
-    destroy = destroy_line.split(',')
-
-    # 创建节点列表
-    nodes = [Node(tokens[i], build[i], destroy[i]) for i in range(len(tokens))]
-
-    # 创建地图并执行操作
-    my_map = Map(nodes)
-    my_map.connect_nodes()
-    my_map.delete_roads()
-    my_map.print_cost()
+    country = [list(map(int, row)) for row in country_str.split(',')]
+    build = [list(row) for row in build_str.split(',')]
+    destroy = [list(row) for row in destroy_str.split(',')]
     
+    return country, build, destroy
 
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
 
+    def find(self, p):
+        if self.parent[p] != p:
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
 
-main()
+    def union(self, p, q):
+        rootP = self.find(p)
+        rootQ = self.find(q)
+
+        if rootP == rootQ:
+            return False
+
+        if self.rank[rootP] > self.rank[rootQ]:
+            self.parent[rootQ] = rootP
+        elif self.rank[rootP] < self.rank[rootQ]:
+            self.parent[rootP] = rootQ
+        else:
+            self.parent[rootQ] = rootP
+            self.rank[rootP] += 1
+
+        return True
+
+def kruskal(n, country, build, destroy):
+    uf = UnionFind(n)
+    edges = []
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if country[i][j] == 1:
+                cost = letter_to_cost(destroy[i][j])
+                edges.append((i, j, cost, 'destroy'))
+            else:
+                cost = letter_to_cost(build[i][j])
+                edges.append((i, j, cost, 'build'))
+
+    edges = sorted(edges, key=lambda x: x[2])
+
+    mst_weight = 0
+    connected_components = n
+
+    for u, v, weight, action in edges:
+        if uf.union(u, v):
+            mst_weight += weight
+            connected_components -= 1
+
+    if connected_components > 1:
+        return float('inf')
+    
+    return mst_weight
+
+def process_input():
+    input_string = input()
+    country, build, destroy = parse_input(input_string)
+    n = len(country)
+
+    mst_weight = kruskal(n, country, build, destroy)
+
+    print(f"{mst_weight}")
+
+process_input()
